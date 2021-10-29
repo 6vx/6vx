8514e9f5636d53adebf1fc533efc2513fa3fe58e
Tim Godin <support@kitebot.ca>
2021-10-28 22:05:05 -0600

    I want to see the deno deploy boot time on a request.

--- oak.ts
+++ oak.ts
@@ -30,11 +30,80 @@ interface Place {
   NPC?: NPC;
 }
 
+interface BagSpawnOptions {
+  colorList: string[];
+  bagLengthLimit: number;
+  maxMultiplier: number;
+  chanceLit: number;
+  chanceColor: number;
+  chanceLevelBumped: number;
+}
+
+interface Tile {
+  uuid?: string;
+  letter: string;
+  maxquantity?: number;
+  baselevel?: number;
+  level?: number;
+  color?: string;
+  multiplier?: number;
+  isLit?: boolean;
+}
+
+interface spaceBuffs {
+  chanceDoubleLetter: number;
+  chanceTripleLetter: number;
+  chanceDoubleWord: number;
+  chanceTripleWord: number;
+  chanceLit: number;
+}
+
 interface Space {
   x: number;
   y: number;
-  place?: Place,
-  highScore?: number,
+  place?: Place;
+  highScore?: number;
+  leaderboard?: string;
+  id?: string;
+  tile?: Tile;
+  isLit?: boolean;
+  isDoubleLetter?: boolean;
+  isTripleLetter?: boolean;
+  isDoubleWord?: boolean;
+  isTripleWord?: boolean;
+}
+
+function assignSpaceBuffs () {
+  masterMap.forEach(space => {
+    // randomly decide if chanceDoubleLetter
+    if (Math.random() < spaceBuffSettings.chanceDoubleLetter) {
+      space.isDoubleLetter = true
+    }
+    // randomly decide if chanceTripleLetter
+    if (Math.random() < spaceBuffSettings.chanceTripleLetter) {
+      space.isTripleLetter = true
+    }
+    // randomly decide if chanceDoubleWord
+    if (Math.random() < spaceBuffSettings.chanceDoubleWord) {
+      space.isDoubleWord = true
+    }
+    // randomly decide if chanceTripleWord
+    if (Math.random() < spaceBuffSettings.chanceTripleWord) {
+      space.isTripleWord = true
+    }
+    // randomly decide if chanceLit
+    if (Math.random() < spaceBuffSettings.chanceLit) {
+      space.isLit = true
+    }
+  })
+}
+
+let spaceBuffSettings:spaceBuffs = {
+  chanceDoubleLetter: 0.05,
+  chanceTripleLetter: 0.02,
+  chanceDoubleWord: 0.008,
+  chanceTripleWord: 0.004,
+  chanceLit: 0.01
 }
 
 let mapAttributes = {
@@ -44,9 +113,7 @@ let mapAttributes = {
   y_max: 500,
 }
 
-let masterMap:Space[] = [
-
-]
+let masterMap:Space[] = [];
 
 let places:Place[] = [
   {
@@ -68,7 +135,8 @@ let places:Place[] = [
 function createMap() {
   for (let x = 0; x < mapAttributes.x_max; x++) {
     for (let y = 0; y < mapAttributes.y_max; y++) {
-      masterMap.push({x: x, y: y})
+      masterMap.push({x: x, y: y, id: crypto.randomUUID()})
+      // masterMap.push({x: x, y: y})
     }
   }
 }
@@ -92,11 +160,12 @@ function addPlaces() {
 function bootMap() {
   createMap();
   addPlaces();
+  assignSpaceBuffs();
 }
 // turning this off for now. Because it's too heavy to run on a deno deploy
 // needs an actual server to run on. Very fucking rad. Will update every time I add new content. So. Sick.
 
-// bootMap();
+bootMap();
 
 // find mastermap location where x is 241 and y is 250
 // and console.log the place
@@ -106,31 +175,33 @@ console.log(masterMap.find(entry => {
   }
 }))
 
-const books = new Map<string, any>();
-books.set("1", {
-  id: "1",
-  title: "The Hound of the Baskervilles",
-  author: "Conan Doyle, Arthur",
-});
+// example of how to use the oak script 1/2
+// const books = new Map<string, any>();
+// books.set("1", {
+//   id: "1",
+//   title: "The Hound of the Baskervilles",
+//   author: "Conan Doyle, Arthur",
+// });
 
 const router = new Router();
 router
   .get("/", (context) => {
-    context.response.body = "Hiya.";
+    context.response.body = "Try /location/x/y (where both are less than 500)";
   })
-  .get("/book", (context) => {
-    context.response.body = Array.from(books.values());
-  })
-  .get("/book/:id", (context) => {
-    if (context.params && context.params.id && books.has(context.params.id)) {
-      context.response.body = books.get(context.params.id);
-    }
-  })
+  // example of how to use the oak script 2/2
+  // .get("/book", (context) => {
+  //   context.response.body = Array.from(books.values());
+  // })
+  // .get("/book/:id", (context) => {
+  //   if (context.params && context.params.id && books.has(context.params.id)) {
+  //     context.response.body = books.get(context.params.id);
+  //   }
+  // })
   .get("/location/:x/:y", (context) => {
     if (context.params && context.params.x && context.params.y) {
       // find the space in masterMap where x and y are equal to context.params.x and context.params.y
       context.response.body = masterMap.filter(function(result) {
-        return result.x === Number(context.params.x) && result.y === Number(context.params.y);
+        return result.x === Number(context.params.x) && result.y === Number(context.params.y);        
       })
 
       // context.response.body = masterMap.find(entry => {
